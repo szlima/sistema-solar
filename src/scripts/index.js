8b487c0e4ada3b5f003ffb2370f260c6b374b877
szlima <suzanelima@outlook.com>
2024-04-05 14:27:22 -0300

    Carregamento dos dados da janela de informações de um arquivo json

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,18 +1,42 @@
+const UNIVERSE= document.querySelector('#universe');
+const MODAL= document.querySelector('#modal');
+const MODAL_CONTENT= document.querySelector("#modal-content");
 const BUTTON_OPEN_MODAL= document.querySelector('#open-button-modal');
 const BUTTON_CLOSE_MODAL= document.querySelector('#close-button-modal');
-const BOX_INFO= document.querySelector('#info-box');
-const UNIVERSE= document.querySelector('#universe');
 
-BUTTON_OPEN_MODAL.addEventListener('click', () => {
-    BOX_INFO.classList.remove('invisible');
+let data;
 
-    if(window.matchMedia('(max-width: 576px)').matches)
-        UNIVERSE.classList.add('invisible');
-});
+BUTTON_OPEN_MODAL.onclick= () => {
+    let content= '';
 
-BUTTON_CLOSE_MODAL.addEventListener('click', () => {
-    BOX_INFO.classList.add('invisible');
+    content+= `<h1>${data.simulation['main-title']}</h1>`;
+    content+= data.simulation['description'].map(p => `<p>${p}</p>`).join('');
+    content+= `<h3 id="dev-info">${data.simulation['dev-info']['info'][0]} <i class="fa-solid fa-heart"></i> ${data.simulation['dev-info']['info'][1]} <a target="_blank" href=${data.simulation['dev-info']['url']}>${data.simulation['dev-info']['text']}</a>.</h3>`;
+    content+= `<h2>${data.simulation['reference-title']}</h2>`;
+    content+= data.simulation['references'].map(a => `<a target="_blank" href=${a.url}>${a.text}</a>`).join('');
+
+    openModal(content);
+};
+
+BUTTON_CLOSE_MODAL.onclick= () => {
+    MODAL.classList.add('invisible');
 
     if(UNIVERSE.classList.contains('invisible'))
         UNIVERSE.classList.remove('invisible');
-});
+};
+
+window.onload= () => setData('english');
+
+function openModal(content){
+    MODAL.classList.remove('invisible');
+
+    if(window.matchMedia('(max-width: 576px)').matches)
+        UNIVERSE.classList.add('invisible');
+
+    MODAL_CONTENT.innerHTML= `${content}`;
+}
+
+async function setData(chosenLanguage){
+    const response= await fetch(`src/json/${chosenLanguage}.json`);
+    data= await response.json();
+}
