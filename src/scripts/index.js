1e7d3427c3ed85d4ed5f727db24003c033a16cff
szlima <suzanelima@outlook.com>
2024-05-10 11:03:13 -0300

    Melhorias na nomeação e uso de classes, IDs e variáveis CSS.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,17 +1,17 @@
 const BODY= document.querySelector('body');
-const TITLE= document.querySelector('header h1');
+const HEADER__TITLE= document.querySelector('#header__title');
 
 const SOLAR_SYSTEM= document.querySelector('#solar-system');
 const SUN= document.querySelector('#sun');
-const PLANETS= Array.from(document.querySelectorAll('.orbit'));
+const PLANETS= Array.from(document.querySelectorAll('.solar-system__orbit'));
 
 const LANGUAGES= document.querySelector('#languages');
-const LANGUAGES_BUTTON= document.querySelector('#languages-button');
-const LANGUAGES_BOX= document.querySelector('#languages-box');
+const LANGUAGES__BUTTON= document.querySelector('#languages__button');
+const LANGUAGES__BOX= document.querySelector('#languages__box');
 
-const MODAL_BOX_CONTENT= document.querySelector("#modal-box-content");
-const MODAL_BOX_OPEN= document.querySelector('#modal-box-open');
-const MODAL_BOX_CLOSE= document.querySelector('#modal-box-close');
+const MODAL__CONTENT= document.querySelector("#modal__content");
+const MODAL__BUTTON__OPEN= document.querySelector('#modal__button--open');
+const MODAL__BUTTON__CLOSE= document.querySelector('#modal__button--close');
 
 let data;
 
@@ -19,15 +19,15 @@ let data;
 
 window.onload= () => {
     setData('portuguese');
-    LANGUAGES_BOX.options.selectedIndex= 1;
+    LANGUAGES__BOX.options.selectedIndex= 1;
 };
 
-TITLE.onclick= () => {
-    const element= 'solar-system';
+HEADER__TITLE.onclick= () => {
     let content= '';
+    const element= 'solar-system';
 
-    content+= `<h1>${data[element].title}</h1>`;
-    content+= data[element].description.map(p => `<p>${p}</p>`).join('');
+    content+= `<h1 class='modal__title--main'>${data[element].title}</h1>`;
+    content+= data[element].description.map(p => `<p class='modal__paragraph'>${p}</p>`).join('');
 
     openModal(content);
 };
@@ -37,16 +37,16 @@ SOLAR_SYSTEM.onmousemove= () => pauseAnimation();
 SOLAR_SYSTEM.onmouseout= () => playAnimation();
 
 SUN.onclick= e => {
-    const element= e.target.id;
     let content= '';
+    const element= e.target.id;
 
-    content+= `<h1>${data[element].title}</h1>`;
-    content+= data[element].description.map(p => `<p>${p}</p>`).join('');
-    content+= '<ul>';
-    content+= `<li>${data[element]['orbital-period'].title}: ${data[element]['orbital-period'].value} ${data[element]['orbital-period'].measure}</li>`;
-    content+= `<li>${data[element]['diameter'].title}: ${data[element]['diameter'].value}</li>`;
-    content+= `<li>${data[element]['obliquity-to-orbit'].title}: ${data[element]['obliquity-to-orbit'].value} ${data[element]['obliquity-to-orbit'].measure}</li>`;
-    content+= `<li>${data[element]['mean-temperature'].title}: ${data[element]['mean-temperature'].value}</li>`;
+    content+= `<h1 class='modal__title--main'>${data[element].title}</h1>`;
+    content+= data[element].description.map(p => `<p class='modal__paragraph'>${p}</p>`).join('');
+    content+= `<ul class='modal__list'>`;
+    content+= `<li class='modal__list-item'>${data[element]['orbital-period'].title}: ${data[element]['orbital-period'].value} ${data[element]['orbital-period'].measure}</li>`;
+    content+= `<li class='modal__list-item'>${data[element]['diameter'].title}: ${data[element]['diameter'].value}</li>`;
+    content+= `<li class='modal__list-item'>${data[element]['obliquity-to-orbit'].title}: ${data[element]['obliquity-to-orbit'].value} ${data[element]['obliquity-to-orbit'].measure}</li>`;
+    content+= `<li class='modal__list-item'>${data[element]['mean-temperature'].title}: ${data[element]['mean-temperature'].value}</li>`;
     content+= '</ul>';
 
     openModal(content);
@@ -54,52 +54,56 @@ SUN.onclick= e => {
 
 PLANETS.map(e => {
     e.onclick= () => {
-        const element= e.id;
-        BODY.classList.add(`modal-${element}`);
         let content= '';
+        const element= e.id;
+        BODY.classList.add(`modal--${element}`);
 
-        content+= `<h1>${data[element].title}</h1>`;
-        content+= data[element].description.map(p => `<p>${p}</p>`).join('');
-        content+= '<ul>';
-        content+= `<div class='selected-item selected-${element}'></div>`;
-        content+= `<li>${data[element]['rotation-period'].title}: ${data[element]['rotation-period'].value} ${data[element]['rotation-period'].measure}</li>`;
-        content+= `<li>${data[element]['orbital-period'].title}: ${data[element]['orbital-period'].value} ${data[element]['orbital-period'].measure}</li>`;
-        content+= `<li>${data[element]['gravity'].title}: ${data[element]['gravity'].value}</li>`;
-        content+= `<li>${data[element]['diameter'].title}: ${data[element]['diameter'].value}</li>`;
-        content+= `<li>${data[element]['distance-from-sun'].title}: ${data[element]['distance-from-sun'].value}</li>`;
-        content+= `<li>${data[element]['obliquity-to-orbit'].title}: ${data[element]['obliquity-to-orbit'].value} ${data[element]['obliquity-to-orbit'].measure}</li>`;
-        content+= `<li>${data[element]['mean-temperature'].title}: ${data[element]['mean-temperature'].value}</li>`;
-        content+= `<li>${data[element]['ring-system'].title}: ${data[element]['ring-system'].value}</li>`;
-        content+= `<li>${data[element]['number-of-moons'].title}: ${data[element]['number-of-moons'].value}</li>`;
+        const patternPlanetaryRings= /jupiter|saturn|uranus|neptune/;
+        const classListModalPlanet= `modal__planet modal__planet--${element}` +
+            (patternPlanetaryRings.test(element) ? ' modal__planet--rings' : '');
+
+        content+= `<h1 class='modal__title--main'>${data[element].title}</h1>`;
+        content+= data[element].description.map(p => `<p class='modal__paragraph'>${p}</p>`).join('');
+        content+= `<ul class='modal__list'>`;
+        content+= `<div class='${classListModalPlanet}'></div>`;
+        content+= `<li class='modal__list-item'>${data[element]['rotation-period'].title}: ${data[element]['rotation-period'].value} ${data[element]['rotation-period'].measure}</li>`;
+        content+= `<li class='modal__list-item'>${data[element]['orbital-period'].title}: ${data[element]['orbital-period'].value} ${data[element]['orbital-period'].measure}</li>`;
+        content+= `<li class='modal__list-item'>${data[element]['gravity'].title}: ${data[element]['gravity'].value}</li>`;
+        content+= `<li class='modal__list-item'>${data[element]['diameter'].title}: ${data[element]['diameter'].value}</li>`;
+        content+= `<li class='modal__list-item'>${data[element]['distance-from-sun'].title}: ${data[element]['distance-from-sun'].value}</li>`;
+        content+= `<li class='modal__list-item'>${data[element]['obliquity-to-orbit'].title}: ${data[element]['obliquity-to-orbit'].value} ${data[element]['obliquity-to-orbit'].measure}</li>`;
+        content+= `<li class='modal__list-item'>${data[element]['mean-temperature'].title}: ${data[element]['mean-temperature'].value}</li>`;
+        content+= `<li class='modal__list-item'>${data[element]['ring-system'].title}: ${data[element]['ring-system'].value}</li>`;
+        content+= `<li class='modal__list-item'>${data[element]['number-of-moons'].title}: ${data[element]['number-of-moons'].value}</li>`;
         content+= '</ul>';
 
         openModal(content);
     };
 });
 
-LANGUAGES_BUTTON.onclick= () => LANGUAGES.classList.toggle('languages-on');
+LANGUAGES__BUTTON.onclick= () => LANGUAGES.classList.toggle('languages--on');
 
-LANGUAGES_BOX.onchange= () => {
-    setData(LANGUAGES_BOX.value);
-    LANGUAGES.classList.remove('languages-on');
+LANGUAGES__BOX.onchange= () => {
+    setData(LANGUAGES__BOX.value);
+    LANGUAGES.classList.remove('languages--on');
 };
 
-MODAL_BOX_OPEN.onclick= () => {
+MODAL__BUTTON__OPEN.onclick= () => {
     let content= '';
 
-    content+= `<h1>${data.simulation['main-title']}</h1>`;
-    content+= data.simulation['description'].map(p => `<p>${p}</p>`).join('');
-    content+= `<h3 id="dev-info">${data.simulation['dev-info']['info'][0]} <i class="fa-solid fa-heart"></i> ${data.simulation['dev-info']['info'][1]} <a target="_blank" href=${data.simulation['dev-info']['url']}>${data.simulation['dev-info']['text']}</a>.</h3>`;
-    content+= `<h2>${data.simulation['reference-title']}</h2>`;
-    content+= data.simulation['references'].map(a => `<a target="_blank" href=${a.url}>${a.text}</a>`).join('');
+    content+= `<h1 class='modal__title--main'>${data.simulation['main-title']}</h1>`;
+    content+= data.simulation['description'].map(p => `<p class='modal__paragraph'>${p}</p>`).join('');
+    content+= `<h3 class='modal__dev'>${data.simulation['dev-info']['info'][0]} <i class="fa-solid fa-heart icon"></i> ${data.simulation['dev-info']['info'][1]} <a class='modal__url' target="_blank" href=${data.simulation['dev-info']['url']}>${data.simulation['dev-info']['text']}</a>.</h3>`;
+    content+= `<h2 class='modal__title--secondary'>${data.simulation['reference-title']}</h2>`;
+    content+= data.simulation['references'].map(a => `<a class='modal__url' target="_blank" href=${a.url}>${a.text}</a>`).join('');
 
     openModal(content);
 };
 
-MODAL_BOX_CLOSE.onclick= () => BODY.classList.remove(
-    'modal-on', 'modal-mercury', 'modal-venus',
-    'modal-earth', 'modal-mars', 'modal-jupiter',
-    'modal-saturn', 'modal-uranus', 'modal-neptune'
+MODAL__BUTTON__CLOSE.onclick= () => BODY.classList.remove(
+    'modal--on', 'modal--mercury', 'modal--venus',
+    'modal--earth', 'modal--mars', 'modal--jupiter',
+    'modal--saturn', 'modal--uranus', 'modal--neptune'
 );
 
 /* ------- ------- */
@@ -111,13 +115,13 @@ async function setData(chosenLanguage){
 }
 
 function setTitle() {
-    TITLE.innerHTML= data.simulation['main-title'];
+    HEADER__TITLE.innerHTML= data.simulation['main-title'];
     document.title= data.simulation['main-title'];
 };
 
 function openModal(content){
-    BODY.classList.add('modal-on');
-    MODAL_BOX_CONTENT.innerHTML= `${content}`;
+    BODY.classList.add('modal--on');
+    MODAL__CONTENT.innerHTML= `${content}`;
 }
 
 function pauseAnimation(){
